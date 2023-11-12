8ee555f59b0cfda04dcabf651cf2e05e6e6bf2bb
kelvinjjwong <kelvinjjwong@outlook.com>
2023-11-12 22:31:16 +0800

    build version 1.0.10

--- Tests/LoggerFactoryTests/LoggerFactoryTests.swift
+++ Tests/LoggerFactoryTests/LoggerFactoryTests.swift
@@ -9,12 +9,23 @@ final class LoggerFactoryTests: XCTestCase {
         print("==== \(self.description) ====")
         LoggerFactory.append(logWriter: ConsoleLogger())
         LoggerFactory.append(logWriter: FileLogger(pathOfFolder: "~/logs"))
+        LoggerFactory.enable([.info, .error, .warning, .trace])
     }
     
     func testMultipleLoggers() throws {
         
-        let logger = LoggerFactory.get(category: "Test", subCategory: "UnitTest")
+        let logger = LoggerFactory.get(category: "Test", subCategory: "testMultipleLoggers")
             .registerWriter(id: "otherfile", writer: FileLogger(pathOfFolder: "~/logs", filename: "123.log"))
         logger.log("this is a log")
+        logger.log(.trace, "this is a trace")
+    }
+    
+    func testLimitedTypes() throws {
+        
+        let logger = LoggerFactory.get(category: "Test", subCategory: "testLimitedTypes")
+            .registerWriter(id: "otherfile", writer: FileLogger(pathOfFolder: "~/logs", filename: "123.log"))
+            .excludeLoggingLevels([.trace])
+        logger.log("this is a log")
+        logger.log(.trace, "this is a trace")
     }
 }
