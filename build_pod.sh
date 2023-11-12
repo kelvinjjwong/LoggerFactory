1f98195984d7cdd5c785080a38774cd824b2a1d7
kelvinjjwong <kelvinjjwong@outlook.com>
2023-11-12 19:28:29 +0800

    build version 1.0.5

--- build_pod.sh
+++ build_pod.sh
@@ -35,20 +35,33 @@ if [[ $versionChange -ne 0 ]]; then
     sed -i .bak -e 's/s.version     = ".*"/s.version     = "'$NEW_VERSION'"/' $PODSPEC; rm -f $PODSPEC.bak
     sed -i .bak -e 's/"'$CURRENT_VERSION'"/"'$NEW_VERSION'"/g' -e 's/~> '$CURRENT_VERSION'/~> '$NEW_VERSION'/g' README.md; rm -f README.md.bak
 fi
-pod spec lint $PODSPEC
+
+GIT_BRANCH=`git status | grep "On branch" | head -1 | awk -F' ' '{print $NF}'`
+CURRENT_VERSION=`grep s.version $PODSPEC | head -1 | awk -F' ' '{print $NF}' | sed 's/"//g'`
+
+if [[ "$GIT_BRANCH" != "$CURRENT_VERSION" ]]; then
+    git branch $CURRENT_VERSION
+    git checkout $CURRENT_VERSION
+fi
+git commit -am "build version $CURRENT_VERSION"
+if [[ $? -ne 0 ]]; then
+   exit -1
+fi
+
+git push --set-upstream origin $CURRENT_VERSION
+if [[ $? -ne 0 ]]; then
+   exit -1
+fi
+
+pod spec lint $PODSPEC --allow-warnings
 if [[ $? -eq 0 ]]; then
-    CURRENT_VERSION=`grep s.version $PODSPEC | head -1 | awk -F' ' '{print $NF}' | sed 's/"//g'`
     SOURCE_URL=`grep s.source $PODSPEC | head -1 | awk -F'"' '{print $2}' | sed 's/.\{4\}$//'`
     echo "If success, you can then:"
     echo
-    echo "1 # push changes of [$CURRENT_VERSION] to git repository"
-    echo "git commit -am \"build new version\""
-    echo "git push"
-    echo
-    echo "2 # publish new release by tagging new version [$CURRENT_VERSION] in git repository"
+    echo "1 # publish new release by tagging new version [$CURRENT_VERSION] in git repository"
     echo "$SOURCE_URL/releases"
     echo ""
-    echo "3 # push new version to Cocoapods trunk"
+    echo "2 # push new version to Cocoapods trunk"
     echo "pod trunk push $PODSPEC"
     echo
 fi
